b16b1fc4f32a41772985420cab9f7d777487f760
NicolasBeltran70 <65606441+NicolasBeltran70@users.noreply.github.com>
2020-12-22 15:46:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -177,9 +177,14 @@ async def create_calendario(calendario: CalendarioInDB):
 
 #######################################################################################################################
 
-origins=[
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080"
-    ]
+from fastapi.middleware.cors import CORSMiddleware
+
+origins=["http://localhost.tiangolo.com",
+         "https://localhost.tiangolo.com",
+         "http://localhost",
+         "http://localhost:8080"]
+
+api.add_middleware(
+    CORSMiddleware, allow_origins=origins, allow_credentials = True,
+    allow_methods=["*"], allow_headers=["*"],
+    )
