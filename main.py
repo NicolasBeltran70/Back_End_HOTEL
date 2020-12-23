528f786bd6b76b2c19a3834e4ac6a3a356eeba2f
NicolasBeltran70 <65606441+NicolasBeltran70@users.noreply.github.com>
2020-12-23 17:03:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -195,7 +195,8 @@ from fastapi.middleware.cors import CORSMiddleware
 origins=["http://localhost.tiangolo.com",
          "https://localhost.tiangolo.com",
          "http://localhost",
-         "http://localhost:8080"]
+         "http://localhost:8080",
+        "https://backh0tel.herokuapp.com"]
 
 api.add_middleware(
     CORSMiddleware, allow_origins=origins, allow_credentials = True,
